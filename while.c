a23379feac80c1aaa91c7e03aa0747bcb247e819
nizamali <nizamali2680@csmu.ac.in>
2022-11-17 09:58:49 +0530

    thurs update

---
+++ while.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{	 
+	// local variable definition
+	int i=10;
+	char
+	
+	
+	// do-while loop execution
+	do
+	{
+		printf("%d\n",i);
+		++i;
+	}
+	while(i<=20);
+	return 0;
+}
